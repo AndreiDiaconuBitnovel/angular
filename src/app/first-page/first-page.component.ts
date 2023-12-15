409d07df609dd375caae1742543ebf10c1e3d20e
Zylok <sergiu.state10@gmail.com>
2023-12-15 17:50:36 +0200

    First Page style added

--- src/app/first-page/first-page.component.ts
+++ src/app/first-page/first-page.component.ts
@@ -15,22 +15,28 @@ interface Feature {
   styleUrls: ['./first-page.component.css'],
 })
 export class FirstPageComponent implements OnInit {
+  title: string = 'Welcome to Translation Kingdom';
+  bigDescription: string =
+    'Discover the amazing features of translation that transcends the realm of international languages! (up to 77 languages)';
   testDbArray: TestDbDto[] = [] as TestDbDto[];
   features: Feature[] = [
     {
-      imgSrc: '../assets/image1.png',
-      title: 'Feature 1',
-      description: 'Description of feature 1.',
+      imgSrc: '../assets/logo-translation.png',
+      title: 'Normal Translation',
+      description:
+        'Your everyday translation that you could find on Google Translate, Microsoft Translator, etc. Maybe a bit quicker but not as large as the kingdom!',
     },
     {
-      imgSrc: '../assets/image2.jpg',
-      title: 'Feature 2',
-      description: 'Description of feature 2.',
+      imgSrc: '../assets/ibm_watson_language_translator.png',
+      title: 'International Translation',
+      description:
+        'The kingdom specialty, a translation that transcends normal translators! It may take a bit longer, but any of the 77 languages are at the push of a button.',
     },
     {
-      imgSrc: '../assets/image3.png',
-      title: 'Feature 3',
-      description: 'Description of feature 3.',
+      imgSrc: '../assets/logo-history.png',
+      title: 'History',
+      description:
+        'Any translation you make is stored in our database, so it can be accessed at any time. You know, in case your dog ate your homework!',
     },
   ];
   constructor(private router: Router, private testDbService: TestDbService) {}
@@ -42,6 +48,17 @@ export class FirstPageComponent implements OnInit {
   onColumnClick(columnNumber: number): void {
     console.log(`Column ${columnNumber} clicked`);
     // Add your logic here for the click event
+    switch (columnNumber) {
+      case 0:
+        this.router.navigate(['/secondPage']);
+        break;
+      case 1:
+        this.router.navigate(['/secondPage']);
+        break;
+      case 2:
+        this.router.navigate(['/thirdPage']);
+        break;
+    }
   }
 
   goToSecondPage() {
